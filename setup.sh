e5fb5d480a7ae672e892485daa9f7a3d435b581c
mkp95 <mkp95@users.noreply.github.com>
2022-12-16 14:42:19 +0530

    Update setup.sh
    Convert to Function

--- setup.sh
+++ setup.sh
@@ -1,62 +1,93 @@
-#install packages
-apt update
-apt -y upgrade
-clear
-echo "Installing Required Programs"
+install(){
+header
+setup
+setup_firewall
+setup_dropbear
+install_badvpn
+install_cert
+install_socket
+set_user
+setup_stunnel
 
+echo "Reboot and Enjoy!!!"
+}
+header(){
+echo "SSH over Websocket Server Setup"
+echo "https://github.com/mkp95/ppws"
+echo ""
+}
+
+setup(){
+echo "Installing Required Programs"
+apt update && apt -y upgrade
 apt -y install stunnel4 nodejs dropbear cmake make build-essential unzip certbot ufw gcc
-clear
+}
+
+setup_firewall(){
 echo "Setting up Firewall"
-#setup firewall
 ufw allow 22
 ufw allow 80
 ufw allow 443
 ufw enable
-echo "Setting up Dropbear"
-#setup dropbear
-wget -O dropbear "https://raw.githubusercontent.com/mkp95/ppws/main/dropbear"
-mv dropbear /etc/default/dropbear
+}
 
+setup_dropbear(){
+echo "Setting up Dropbear"
+service dropbear stop
+service dropbear start
+service dropbear stop
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=40000/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_BANNER=""/DROPBEAR_BANNER="/etc/banner.txt"/g' /etc/default/dropbear
+echo 'Get Ready Babyyyy!' > /etc/banner.txt
+service dropbear start
+}
+
+install_badvpn(){
 echo "Setting up BadVPN"
-#install badvpn
 wget -O badvpn.zip "https://codeload.github.com/mkp95/badvpn/zip/refs/heads/master"
 unzip badvpn.zip
 cd badvpn-master
 mkdir build && cd build
 cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
 make install
-wget -O badvpn.service  "https://raw.githubusercontent.com/mkp95/ppws/main/badvpn.service"
+cd ../..
+rm -r *
+wget -O /etc/systemd/system/badvpn.service  "https://raw.githubusercontent.com/mkp95/ppws/main/badvpn.service"
+systemctl enable badvpn.service
+}
 
-echo "Setting up Tunnel"
-#setup certs for stunnel
-wget -O stunnel.conf "https://raw.githubusercontent.com/mkp95/ppws/main/stunnel.conf"
-mv stunnel.conf /etc/stunnel.conf
-echo "Enter Domain to be used(make sure it has 'A' record with IP of this Server): "
+install_cert(){
+echo "Enter domain: "
 read domain
-certbot certonly --standalone --preferred-challenges http -d $domain
+certbot certonly --standalone --preferred-challenges http -d in1.techtrek.info -m a@$domain --noninteractive --agree-tos
 cp /etc/letsencrypt/live/$domain/privkey.pem  /etc/stunnel
 cp /etc/letsencrypt/live/$domain/cert.pem /etc/stunnel/
 chmod 600 /etc/stunnel/*.pem
+}
 
-
+install_socket(){
 echo "WebSocket Setup"
-#setup nodews
 mkdir /etc/nodews
 wget -O /etc/nodews/proxy3.js "https://raw.githubusercontent.com/mkp95/ppws/main/proxy3.js"
-wget -O nodews.service "https://raw.githubusercontent.com/mkp95/ppws/main/nodews.service"
-
-#enable services
-cp *.service /etc/systemd/system/
+wget -O /etc/systemd/system/nodews.service "https://raw.githubusercontent.com/mkp95/ppws/main/nodews.service"
 systemctl enable nodews.service
-systemctl enable badvpn.service
+}
 
+set_user(){
+echo "Setting up User for SSH"
 echo "Enter Username: "
 read user
-echo "Enter Password: "
-read pass
-#setup blank shell
+echo "Enter Pass: "
+read $pass
 grep -Fx -q "/bin/false" /etc/shells || echo "/bin/false" >> /etc/shells
-
-#add new user
+grep -Fx -q "/usr/sbin/nologin" /etc/shells || echo "/usr/sbin/nologin" >> /etc/shells
 useradd -M $user -s /bin/false
 chpasswd <<<"$user:$pass"
+}
+
+setup_stunnel(){
+echo "Setting up Tunnel"
+wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/mkp95/ppws/main/stunnel.conf"
+stunnel
+}
